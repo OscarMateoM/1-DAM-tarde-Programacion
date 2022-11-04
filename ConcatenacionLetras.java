9e5c0ed39179e67f6bfafe4fdba375f259543ab2
OscarMateoM <117314197+OscarMateoM@users.noreply.github.com>
2022-11-04 16:39:16 +0100

    Add files via upload

---
+++ ConcatenacionLetras.java
@@ -0,0 +1,39 @@
+/*
+ * ConcatenacionLetras.java
+ * 
+ * AUTOR: OSCAR MATEO MARTIN
+ * 
+ * Copyright 2022 oski0 <oski0@LAPTOP-9H8ODA6N>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+public class ConcatenacionLetras {
+	
+	public static void main (String[] args) {
+		char letra1 = 'a';
+		char letra2= 'b';
+		
+		System.out.println(letra1);
+		System.out.println(letra2);
+		System.out.println(letra1 + letra2);
+		System.out.println(letra1 + "" + letra2);
+	}
+}
+
