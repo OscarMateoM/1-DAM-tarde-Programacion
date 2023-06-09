95c0d547830175b09082bce634c102345d2ceff4
Oscar <omatmar964@g.educaand.es>
2023-06-09 19:15:33 +0200

    Ej18 Arrays

--- Recuperacion/segundaEvaluacion/C7/Arrays Unidimensionales/Ej18.java
+++ Recuperacion/segundaEvaluacion/C7/Arrays Unidimensionales/Ej18.java
@@ -8,8 +8,53 @@
  * números que queden.
  */
 public class Ej18 {
+
     public static void main(String[] args) {
-       
+        int[] numeros = new int[10];
+        int[] menores = new int[10];
+        int[] mayores = new int[10];
+        int cuentaMenores = 0;
+        int cuentaMayores = 0;
+
+        // Genera el array original y clasifica los números
+        for (int i = 0; i < 10; i++) {
+            numeros[i] = (int) (Math.random() * 201);
+
+            if (numeros[i] <= 100) {
+                menores[cuentaMenores++] = numeros[i];
+            } else {
+                mayores[cuentaMayores++] = numeros[i];
+            }
+        }
+
+        System.out.println("Array original:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Índice " + i + ": " + numeros[i]);
+        }
+
+        // Genera el array resultado
+        int[] resultado = new int[10];
+        int menoresColocados = 0;
+        int mayoresColocados = 0;
+
+        int j = 0;
+        do {
+
+            if (menoresColocados < cuentaMenores) {
+                resultado[j++] = menores[menoresColocados++];
+            }
+
+            if (mayoresColocados < cuentaMayores) {
+                resultado[j++] = mayores[mayoresColocados++];
+            }
+
+        } while (j < 10);
+
+        System.out.println("\nArray resultado:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Índice " + i + ": " + resultado[i]);
+        }
     }
 }
+
 
